ef97b45086f787f718f506c39f27f8510647aee9
Aditi-jha2120 <67851102+Aditi-jha2120@users.noreply.github.com>
2025-03-13 19:39:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let tasks = [];
+
+const addTask = ()=>{
+const taskInput = document.getElementById('taskInput');
+const text = taskInput.value.trim();
+
+if(text){
+    tasks.push({text: text, completed: false});
+
+    updateTasksList();
+}
+console.log(tasks);
+};
+const updateTasksList = ()=>{
+    const taskList = document.getElementById('task-list');
+    taskList.innerHTML = '';
+
+    tasks.forEach(task =>{
+        const listItem = document.createElement('li');
+
+        listItem.innerHTML = `
+        `
+    })
+}
+document.getElementById('newTask').addEventListener('click', function(e){
+   e.preventDefault()
+   
+   addTask();
+});
